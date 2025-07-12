fa5504788e019353e53c0f09ac0a1fe9cf53e07a
antoniozamora2002 <antoniozp02@gmail.com>
2025-07-12 09:18:29 -0500

    edit config typeorm, addded database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,18 @@
 import { Module } from '@nestjs/common';
 import { BooksModule } from './books/books.module';
 import { ReviewsModule } from './reviews/reviews.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Book } from './books/entities/book.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { Review } from './reviews/entities/review.entity';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // Hace que esté disponible en todos los módulos sin importar
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'mysql',
-        host: config.get('DB_HOST'),
-        port: parseInt(config.get('DB_PORT') ?? '3306', 10),
-        username: config.get('DB_USERNAME'),
-        password: config.get('DB_PASSWORD'),
-        database: config.get('DB_DATABASE'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
-    }),
+    DatabaseModule,
     BooksModule,
     ReviewsModule,
+    DatabaseModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/database/typeorm.config.ts
@@ -0,0 +1,16 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const getTypeOrmConfig = async (
+  configService: ConfigService,
+): Promise<TypeOrmModuleOptions> => ({
+  type: 'mysql',
+  host: configService.get('DB_HOST'),
+  port: parseInt(configService.get('DB_PORT') ?? '3306', 10),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_DATABASE'),
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: false, // False en producción!
+  logging: false, // muestra logs de SQL
+});
